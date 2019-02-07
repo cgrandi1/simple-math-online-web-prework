04537c976a71032e58404c90af72745ac86813f2
astronomical-form-6016 <carltondgrandison@gmail.com>
2019-02-07 03:46:22 +0000

    Automatically backed up by Learn

--- lib/math.rb
+++ lib/math.rb
@@ -34,7 +34,9 @@ def square_root(num)
 end
 
 def order_of_operation(num1, num2, num3, num4)
-  
-  #Hint:  __ + (( __ * __ ) / __ )
+  num1 = 7
+  num2 = 43
+  num3 = 23
+  num4 = 83
+  #Hint:  num1 + (( num2 * num3 ) / num4 )
 end
-7, 43, 23, 83) eq 18
