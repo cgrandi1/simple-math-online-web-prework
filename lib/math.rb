de3170fc2a43d32e74faefcd1b9739e312e2ef7b
astronomical-form-6016 <carltondgrandison@gmail.com>
2019-02-07 03:31:36 +0000

    Automatically backed up by Learn

--- lib/math.rb
+++ lib/math.rb
@@ -1,25 +1,25 @@
 def addition(num1, num2)
   num1 = 5
   num2 = 4 
-  return num1 + num2 
+  num1 + num2 
 end
 
 def subtraction(num1, num2)
   num1 = 10 
   num2 = 5
-  return num1 - num2
+  num1 - num2
 end
 
 def division(num1, num2)
   num1 = 50
   num2 = 2 
-  return num1/num2
+  num1/num2
 end
 
 def multiplication(num1, num2)
   num1 = 4 
   num2 = 30
-  return num1 * num2
+  num1 * num2
 end
 
 def modulo(num1, num2)
